d3d09b9d14a4f7f9b5e918be15fb711336d37886
godovih87 <157564259+godovih87@users.noreply.github.com>
2024-12-07 10:17:31 +0500

    Add files via upload

---
+++ Bagazh/Bagazh/Bagazh.cpp
@@ -0,0 +1,25 @@
+﻿#define _CRT_SECURE_NO_WARNINGS //штука для того чтобы файл окрывался в небезопасном режиме(функция fopen небезопасна)
+#include <stdio.h>
+
+int main() {
+    FILE* file;
+    int quantity;
+    float weight;
+
+    file = fopen("C:\\Users\\FostersPC\\Desktop\\Bagazh\\Bagazh.txt", "r");
+    if (file == NULL) {
+        printf("Невозможно открыть файл!\n");
+        return 1;
+    }
+
+    printf("Пассажиры с более чем двумя вещами:\n");
+
+    while (fscanf(file, "%d %f", &quantity, &weight) == 2) {
+        if (quantity > 2) {
+            printf("Количество вещей: %d, Вес багажа: %.2f кг\n", quantity, weight);
+        }
+    }
+
+    fclose(file);
+    return 0;
+}
